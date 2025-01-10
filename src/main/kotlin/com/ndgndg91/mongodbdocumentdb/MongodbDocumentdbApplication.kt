751dc08e6dbab6be34bb463bccd8df4e63c02d58
ndgndg91 <ndgndg91@gmail.com>
2025-01-10 12:27:22 +0900

    init project

---
+++ src/main/kotlin/com/ndgndg91/mongodbdocumentdb/MongodbDocumentdbApplication.kt
@@ -0,0 +1,11 @@
+package com.ndgndg91.mongodbdocumentdb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MongodbDocumentdbApplication
+
+fun main(args: Array<String>) {
+    runApplication<MongodbDocumentdbApplication>(*args)
+}
